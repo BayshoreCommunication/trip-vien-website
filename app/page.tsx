529b69104fda331faaf781e3b11dd99decf314f7
sajolbd <shamim15-9760@diu.edu.bd>
2026-01-18 11:09:34 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-4xl font-bold text-black">Welcome to Trip Vien</h1>
-      <p>Your one-stop solution for all travel need.</p>
+      <p>Your one-stop solution for all travel</p>
     </> 
   );
 }
